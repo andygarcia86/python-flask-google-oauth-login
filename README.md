b54d523bf0412bcb93f2508bbdb509576f339e9c
Andy Garcia <andygarcia86@gmail.com>
2020-05-01 11:54:35 -0500

    Initial setup

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # Python flask google oauth login
 
-Sample demo for login using google account using Python flask
+Sample demo for login in Python flask application using a google account
 
 ## Requirments
 
 1. Python 3.4+
 2. Docker
 3. My SQL
+
+## Setup
+
+1. Run `pip install -r requirements.txt` in the CMD using a Administrator account
+2. 
---
+++ app.py
@@ -0,0 +1,49 @@
+# Python standard libraries
+import json
+import os
+import sqlite3
+
+# Third-party libraries
+from flask import Flask, redirect, request, url_for
+from flask_login import (
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
+)
+from oauthlib.oauth2 import WebApplicationClient
+import requests
+
+# Internal imports
+from db import init_db_command
+from user import User
+
+# Configuration
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
+GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
+
+# Flask app setup
+app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
+
+# User session management setup
+# https://flask-login.readthedocs.io/en/latest
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# Naive database setup
+try:
+    init_db_command()
+except sqlite3.OperationalError:
+    # Assume it's already been created
+    pass
+
+# OAuth 2 client setup
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+# Flask-Login helper to retrieve a user from our db
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
